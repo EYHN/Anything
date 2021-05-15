d2f10e541c7ffea51f7c4101a6c1b8fe4029748e
EYHN <cneyhn@gmail.com>
2021-05-15 18:45:29 +0800

    feat(Utils): Event and EventEmitter

---
+++ OwnHub.Tests.Utils/EventTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using OwnHub.Utils.Event;
+
+namespace OwnHub.Tests.Utils
+{
+    public class EventTest
+    {
+        [Test]
+        [Timeout(3000)]
+        public async Task FeatureTest()
+        {
+            EventEmitter<string> emitter = new();
+            var @event = emitter.Event;
+            var callCount = 0;
+            @event.On(
+                (e) =>
+                {
+                    Assert.AreEqual("hello", e);
+                    callCount++;
+                });
+            @event.On(
+                async (e) =>
+                {
+                    Assert.AreEqual("hello", e);
+                    await Task.Delay(100);
+                    callCount++;
+                });
+            Assert.AreEqual(0, callCount);
+            emitter.Emit("hello");
+            Assert.AreEqual(2, callCount);
+
+            await emitter.EmitAsync("hello");
+            Assert.AreEqual(4, callCount);
+        }
+    }
+}
