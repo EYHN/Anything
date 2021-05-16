1b320bbd0655e53654b8a46a59a3cda7e6b8333c
EYHN <cneyhn@gmail.com>
2021-05-16 10:43:46 +0800

    feat(Preview): mime type rules

---
+++ OwnHub.Preview/MimeType/MimeTypeRules.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text.Json;
+using OwnHub.Utils;
+
+namespace OwnHub.Preview.MimeType
+{
+    public class MimeTypeRules
+    {
+        public record MimeTypeRule(string Mime, string[] Extensions, string Icon);
+
+        public static MimeTypeRules
+            DefaultRules { get; } = FromJson(Resources.ReadEmbeddedTextFile(typeof(MimeTypeRules).Assembly, "Resources/mimetype.json"));
+
+        private readonly MimeTypeRule[] _rules;
+
+        public MimeTypeRules(MimeTypeRule[] rules)
+        {
+            _rules = rules;
+        }
+
+        public static MimeTypeRules FromJson(string json)
+        {
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true, ReadCommentHandling = JsonCommentHandling.Skip };
+            var rules = JsonSerializer.Deserialize<MimeTypeRule[]>(json, options);
+            if (rules == null)
+            {
+                throw new InvalidDataException("JSON data error.");
+            }
+
+            return new MimeTypeRules(rules);
+        }
+
+        public string? Match(Url url)
+        {
+            var extname = PathLib.Extname(url.Path).ToLower();
+            foreach (MimeTypeRule mimetype in _rules)
+            {
+                foreach (var ext in mimetype.Extensions)
+                {
+                    if (ext == extname)
+                    {
+                        return mimetype.Mime;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
