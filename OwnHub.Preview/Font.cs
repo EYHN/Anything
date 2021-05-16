14b34ed58d9752f087b994c3b9569fce09a43ef5
EYHN <cneyhn@gmail.com>
2021-05-16 10:43:25 +0800

    feat(Preview): render context

---
+++ OwnHub.Preview/Font.cs
@@ -0,0 +1,63 @@
+﻿using OwnHub.Utils;
+using SkiaSharp;
+using Topten.RichTextKit;
+
+namespace OwnHub.Preview
+{
+    public static class Font
+    {
+        static Font()
+        {
+            CharacterMatcher.Initialize();
+        }
+
+        public class TextBlock : Topten.RichTextKit.TextBlock
+        {
+        }
+
+        public class CharacterMatcher : ICharacterMatcher
+        {
+            private readonly SKTypeface[] _typeFaces;
+
+            static CharacterMatcher()
+            {
+                SKTypeface[] typeFaces =
+                {
+                    SKTypeface.FromStream(
+                        Resources.ReadEmbeddedFile(typeof(CharacterMatcher).Assembly, "Resources/Fonts/UbuntuMono-R.ttf")),
+                    SKTypeface.FromStream(
+                        Resources.ReadEmbeddedFile(typeof(CharacterMatcher).Assembly, "Resources/Fonts/NotoSansCJKsc-Regular.otf"))
+                };
+                FontFallback.CharacterMatcher = new CharacterMatcher(typeFaces);
+            }
+
+            public CharacterMatcher(SKTypeface[] typeFaces)
+            {
+                _typeFaces = typeFaces;
+            }
+
+            public SKTypeface? MatchCharacter(
+                string familyName,
+                int weight,
+                int width,
+                SKFontStyleSlant slant,
+                string[] bcp47,
+                int character)
+            {
+                foreach (SKTypeface typeFace in _typeFaces)
+                {
+                    if (typeFace.ContainsGlyph(character))
+                    {
+                        return typeFace;
+                    }
+                }
+
+                return null;
+            }
+
+            public static void Initialize()
+            {
+            }
+        }
+    }
+}
