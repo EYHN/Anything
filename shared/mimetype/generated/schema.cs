0ba72991efe29de941488385a2753be24fcfced6
KeJun <kejun1997@gmail.com>
2021-08-20 22:01:58 +0800

    chore(shared): arrangement audio,video,font,text type

--- shared/mimetype/generated/schema.cs
+++ shared/mimetype/generated/schema.cs
@@ -20,38 +20,78 @@ namespace Anything.Preview.MimeType.Schema
 
     public static MimeType image_vnd.microsoft.icon => new(@"image/vnd.microsoft.icon");
 
+    public static MimeType audio_mp4 => new(@"audio/mp4");
+
+    public static MimeType audio_mpeg => new(@"audio/mpeg");
+
+    public static MimeType audio_ogg => new(@"audio/ogg");
+
+    public static MimeType audio_webm => new(@"audio/webm");
+
+    public static MimeType audio_x_aac => new(@"audio/x-aac");
+
+    public static MimeType audio_x_flac => new(@"audio/x-flac");
+
+    public static MimeType audio_x_wav => new(@"audio/x-wav");
+
+    public static MimeType video_mp4 => new(@"video/mp4");
+
+    public static MimeType video_mpeg => new(@"video/mpeg");
+
+    public static MimeType video_ogg => new(@"video/ogg");
+
+    public static MimeType video_quicktime => new(@"video/quicktime");
+
+    public static MimeType video_webm => new(@"video/webm");
+
+    public static MimeType video_x_flv => new(@"video/x-flv");
+
+    public static MimeType video_x_matroska => new(@"video/x-matroska");
+
+    public static MimeType video_x_ms_wmv => new(@"video/x-ms-wmv");
+
+    public static MimeType video_x_msvideo => new(@"video/x-msvideo");
+
+    public static MimeType font_collection => new(@"font/collection");
+
+    public static MimeType font_otf => new(@"font/otf");
+
+    public static MimeType font_ttf => new(@"font/ttf");
+
+    public static MimeType font_woff => new(@"font/woff");
+
+    public static MimeType font_woff2 => new(@"font/woff2");
+
+    public static MimeType text_css => new(@"text/css");
+
+    public static MimeType text_csv => new(@"text/csv");
+
+    public static MimeType text_html => new(@"text/html");
+
     public static MimeType text_plain => new(@"text/plain");
 
-    public static MimeType application_json => new(@"application/json");
+    public static MimeType text_x_c => new(@"text/x-c");
 
-    public static MimeType application_pdf => new(@"application/pdf");
+    public static MimeType text_x_java_source => new(@"text/x-java-source");
 
-    public static MimeType application_javascript => new(@"application/javascript");
+    public static MimeType text_json => new(@"text/json");
 
-    public static MimeType application_typescript => new(@"application/typescript");
+    public static MimeType text_javascript => new(@"text/javascript");
+
+    public static MimeType text_typescript => new(@"text/typescript");
 
     public static MimeType text_x_csharp => new(@"text/x-csharp");
 
     public static MimeType text_x_go => new(@"text/x-go");
 
-    public static MimeType application_x_php => new(@"application/x-php");
-
-    public static MimeType text_x_java => new(@"text/x-java");
-
-    public static MimeType text_x_csrc => new(@"text/x-csrc");
-
-    public static MimeType text_x_chdr => new(@"text/x-chdr");
-
-    public static MimeType text_x_c__src => new(@"text/x-c++src");
-
-    public static MimeType text_x_c__hdr => new(@"text/x-c++hdr");
+    public static MimeType text_x_php => new(@"text/x-php");
 
     public static MimeType text_x_python => new(@"text/x-python");
 
     public static MimeType text_x_pascal => new(@"text/x-pascal");
 
-    public static MimeType text_css => new(@"text/css");
-
     public static MimeType text_markdown => new(@"text/markdown");
+
+    public static MimeType application_pdf => new(@"application/pdf");
   }
 }
